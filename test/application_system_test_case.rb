d184f870587bd5ad3a88a55914a668696471910b
Rafael Mendonça França <rafael@rubyonrails.org>
2023-09-19 23:30:12 +0000

    Make system tests work
    We need to have a container just to run the chromedriver and
    expose the capybara server port so this container can access the
    application.
    
    Using app as the host for the application doesn't work since
    chrome automatically make the request HTTPS.
    
    For a future commit we could try to setup a extra_hosts entry in
    the app service using the `.test` TLD and use it instead since
    it looks better.
    
    Same with the selenium service.

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,11 @@
 require "test_helper"
 
+Capybara.server_host = "rails-app"
+Capybara.server_port = ENV["CAPYBARA_SERVER_PORT"]
+
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  driven_by :selenium, using: :chrome, screen_size: [1400, 1400], options: {
+    browser: :remote,
+    url: "http://selenium:4444",
+  }
 end
